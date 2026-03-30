ce66b22ee41173b2a88e068b2703f9d4a53df5d7
rsdylanr <dylan0531@icloud.com>
2026-03-30 10:11:28 -0400

    Refactor comments and clean up code formatting

--- services/game_sensor_service.py
+++ services/game_sensor_service.py
@@ -1,20 +1,18 @@
 import time
-import discord
 
 class GameSensorService:
     def __init__(self, bot):
         self.bot = bot
-        self.activity_cache = {} # {channel_id: last_active_timestamp}
-        self.inactivity_threshold = 600 # 10 minutes in seconds
+        # {channel_id: last_active_timestamp}
+        self.activity_cache = {} 
+        # Default: 10 minutes of silence stops the games
+        self.inactivity_threshold = 600 
 
     def record_activity(self, channel_id: int):
-        """Called by the dispatcher every time a human speaks."""
+        """Updates the 'Heartbeat' of a channel."""
         self.activity_cache[channel_id] = time.time()
 
     def is_channel_active(self, channel_id: int) -> bool:
-        """Checks if the channel has had a message within the threshold."""
+        """Returns True if someone spoke within the threshold."""
         last_active = self.activity_cache.get(channel_id, 0)
         return (time.time() - last_active) < self.inactivity_threshold
-
-    def set_threshold(self, minutes: int):
-        self.inactivity_threshold = minutes * 60
