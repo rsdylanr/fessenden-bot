9b71948051d6a1df8e53d853b5c47b28a91f033f
rsdylanr <dylan0531@icloud.com>
2026-04-06 14:28:26 -0400

    Create servicetodadd.py

---
+++ docs/servicetodadd.py
@@ -0,0 +1,25 @@
+# this is the type of thing id like for a service
+
+# create something for buttons
+button = get_button(somewaytogetthebutton)
+if not button:
+  assert that there is no button to use
+
+
+def button.create_button(self):
+  self.button_id = button.create_new_button_id
+  button.color = self.button_color
+  button.text = self.button_text
+  button.action = somehow run a function with this button
+  button.locked_status = self.button_locked_status
+  button.attached_message = idk how to do this
+  button.cooldown = self.button_cooldown # we need to do this somehow
+  button.default_error_message = get_default_error_message
+  button.allowed_users = # idk how to do this but make it optinal
+  button.processing_request: bool
+  button.is_link == whether the button has a set url
+  button.url = self.button_url
+
+  def EmbedService.createEmbed(fjafhdsjkfhldaskjf)
+  
+  
